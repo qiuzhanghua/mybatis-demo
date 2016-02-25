33cc584d35b16495e172e05970f783a90bdb8eec
qiuzhanghua <qiuzhanghua@icloud.com>
2016-02-25 12:28:44 +0800

    try to use "data class"

--- src/main/kotlin/com/example/domain/City.kt
+++ src/main/kotlin/com/example/domain/City.kt
@@ -5,23 +5,5 @@ import java.io.Serializable
 /**
  * Created by qiuzhanghua on 16/2/24.
  */
-class City() : Serializable {
-
-  var id: Long? = null
-
-  var name: String? = null
-
-  var state: String? = null
-
-  var country: String? = null
-
-  override fun toString(): String {
-    return "City{" +
-        "id=" + this.id +
-        ", name='" + this.name + '\'' +
-        ", state='" + this.state + '\'' +
-        ", country='" + this.country + '\'' +
-        '}'
-  }
-}
+data class City(var id: Long = 0L, var name:String = "", var state: String = "", var country: String = "")
 
